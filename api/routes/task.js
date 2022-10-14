e65b57d49983e57b55ba093dfa694d19413f785f
tolgabayra <tolgabayra@github.com>
2022-10-14 20:43:20 +0300

    Task routes işlemleri

--- api/routes/task.js
+++ api/routes/task.js
@@ -2,4 +2,10 @@ const router = require("express").Router()
 const TaskController = require("../controllers/TaskController")
 
 
+router.get("/:id",TaskController.getTask)
+router.get("/",TaskController.getAllTask)
+router.post("/", TaskController.createTask)
+router.delete("/:id",TaskController.deleteTask)
+router.put("/:id",TaskController.updateTask)
+
 module.exports = router
